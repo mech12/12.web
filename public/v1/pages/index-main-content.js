c170367f0263cbf36df0da9fe3622571d2b9e204
mech12 <whdnrfo@gmail.com>
2018-12-14 18:55:18 +0900

    2

---
+++ public/v1/pages/index-main-content.js
@@ -0,0 +1,11 @@
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+module('phonecatApp').
+component('indexMainContent', { // `<index-main-content>` element.
+
+    templateUrl: 'index-main-content.html',
+    controller: function () {
+        var ctrl = this;
+    }
+});
---
+++ public/v1/pages/index-right-sidebar12.js
@@ -0,0 +1,11 @@
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+module('phonecatApp').
+component('indexRightSidebar12', { 
+
+    templateUrl: 'index-right-sidebar12.html',
+    controller: function () {
+        var ctrl = this;
+    }
+});
