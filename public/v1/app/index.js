5d7a6fe46b5a78a832f9cc3183a8743a4f0af043
mech12 <whdnrfo@gmail.com>
2018-12-17 19:59:59 +0900

    pages

--- public/v1/app/index.js
+++ public/v1/app/index.js
@@ -1,6 +1,16 @@
 var g_G = {
-	locale : 'kr',
+    locale: 'kr',
+    pages: {
+        'index-main-chat': true,
+        'index-user-info': false,
+    },
+
 };
+
+
+g_G.getString = function(str, locale) {
+    return str;
+}
 
 // Define the `App12ships` module
 g_G.App12ships = angular.module('App12ships', []);
@@ -8,36 +18,26 @@ g_G.App12ships = angular.module('App12ships', []);
 // Define the `Ctlr12ships` controller on the `App12ships` module
 g_G.App12ships.controller('Ctrl12ships', function Ctrl12ships($scope) {
 
-	$scope.g_G = g_G;
-	g_G.Ctrl12ships = $scope;
-	g_G.path_endpoint = g_G.getString('index-main-chat');
+    $scope.g_G = g_G;
+    g_G.Ctrl12ships = $scope;
+    g_G.path_endpoint = g_G.getString('index-main-chat');
 
-	$scope.pageList={
-		'index-main-chat' : true,
-		'index-user-info' : false,
-	};
+    $scope.isShowPage = function(page) {
+        return $scope.pageList[page] == true;
+    }
 
-	$scope.isShowPage = function (page){
-		return $scope.pageList[page]==true;
-	}
+    $scope.showPage = function(page) {
 
-	$scope.showPage = function(page){
+        g_G.path_endpoint = page;
 
-		g_G.path_endpoint = page;
-
-		Object.keys($scope.pageList).forEach(k=>{
-			if(k==page)
-				$scope.pageList[k] = true;
-			else 
-				$scope.pageList[k] = false;
-		});
-	}
+        Object.keys(g_G.pages).forEach(k => {
+            if (k == page)
+                g_G.pages[k] = true;
+            else
+                g_G.pages[k] = false;
+        });
+    }
 
 });
 
 
-
-
-g_G.getString = function (str,locale){
-	return str;
-}
